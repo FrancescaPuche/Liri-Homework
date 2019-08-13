eda1bb7208e5055d817e76380d4da94aa728029a
Francesca P <francescapuche@gmail.com>
2019-08-13 00:14:37 -0400

    intalling npms and adding files

---
+++ liri.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+
+const keys = require("./keys.js");
+
+const Spotify = require('node-spotify-api');
+const axios = require('axios');
+const moment = require('moment');
+const fs = require("fs");
