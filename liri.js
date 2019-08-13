534a5d0bf345f1389b1f85fe63ee51685ebba27b
Francesca P <francescapuche@gmail.com>
2019-08-13 00:21:54 -0400

    variables for saving user's requests

--- liri.js
+++ liri.js
@@ -6,3 +6,6 @@ const Spotify = require('node-spotify-api');
 const axios = require('axios');
 const moment = require('moment');
 const fs = require("fs");
+
+let action = process.argv[2];
+let userRequest = process.argv.slice(3).join(" "); 
